8e821c90ef8e4e7f7b375a4b84e1db18a13a98d8
Aum Zaveri <aumzaveri06@gmail.com>
2024-11-15 16:31:41 -0500

    Employee service and controller updated to good standards.

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,34 +1,103 @@
 const service = require('../services/employeeServices')
 
 // getting all employees from the database
-const getEmployees = (req, res) => {
-    service.getEmployees(req, res)
+const getEmployees = async (req, res) => {
+    try{
+        const employees = await service.getEmployees()
+        res.status(201).json({
+            status: "successful",
+            Employees: employees,
+        });
+    }catch(e){
+        res.status(400).json({
+            status: "unsuccessful",
+            title: e.title,
+            message: e.message
+        });
+    }
 }
 
 // create an employee into the database
-const createEmployee = (req, resolve) => {
-    service.createEmployee(req, res)
+const createEmployee = async (req, res) => {
+    try{
+        const employee = await service.createEmployee(req.body)
+        res.status(201).json({
+            status: "successful",
+            message: "Employee created successfully",
+            employee: employee,
+        });
+    }catch(e){
+        res.status(400).json({
+            status: "unsuccessful",
+            title: e.title,
+            message: e.message
+        });
+    }
 }
 
 // get a employee with the given eid from the employee id
-const getEmployee = (req, res) => {
-    service.getEmployee(req, res)
+const getEmployee = async (req, res) => {
+    try{
+        const employee = await service.getEmployee(req.params.id)
+        res.status(201).json({
+            status: "successful",
+            employee: employee,
+        });
+    }catch(e){
+        res.status(400).json({
+            status: "unsuccessful",
+            title: e.title,
+            message: e.message
+        });
+    }
 }
 
 // update employee information
-const updateEmployee = (req, res) => {
-    service.updateEmployee(req, res)
+const updateEmployee = async (req, res) => {
+    try{
+        const new_emp = await service.updateEmployee(req.params.eid, req.body)
+        res.status(201).json({
+            status: "successful",
+            message: "Employee updated Successfully",
+            employee: new_emp,
+        });
+    }catch(e){
+        res.status(400).json({
+            status: "unsuccessful",
+            title: e.title,
+            message: e.message
+        });
+    }
 }
-
 // delete employee from the given id
-const deleteEmployee = (req, res) => {
-    service.deleteEmployee(req, res)
+const deleteEmployee = async (req, res) => {
+    try{
+        await service.deleteEmployee(req.query.eid)
+        res.status(204).send("Employee deleted Successfully");
+    }catch(e){
+        res.status(400).json({
+            status: "unsuccessful",
+            title: e.title,
+            message: e.message
+        });
+    }
 }
 
-const searchEmployee = (req, res) => {
-    service.searchEmployee(req, rest)
+const searchEmployee = async (req, res) => {
+    try{
+        const employees = await service.searchEmployee(req.params.search_query)
+        res.status(201).json({
+            status: "successful",
+            Employees: employees,
+        });
+    }catch(e){
+        res.status(400).json({
+            status: "unsuccessful",
+            title: e.title,
+            message: e.message
+        });
+    }
 }
-
 // exporting all the functions.
 module.exports = {
     getEmployees,
--- services/employeeServices.js
+++ services/employeeServices.js
@@ -14,106 +14,53 @@ const findEmployeeE = async (empEmail) => {
 }
 
 // getting all the employees in the database
-const getEmployees = async (req, rest) => {
-    try {
-        res.status(200).send(JSON.stringify(await Employee.find({})))
-    } catch (e) {
-        res.error(e)
-    }
+const getEmployees = async () => {
+    return await Employee.find({});
 }
 
 // creating employee from the req body
-const createEmployee = async (req, res) => {
-    try {
-        let { first_name, last_name, email, position, salary, date_of_joining, department } = req.body
-        let emp = await findEmployeeE(email)
-        // if the emp found is null then create new employee
-        if (emp === null) {
-            let new_emp = Employee({ first_name: first_name, last_name: last_name, email: email, position: position, salary: salary, date_of_joining: date_of_joining, department: department })
-            await new_emp.save().then((data) => {
-                res.status(201).send(JSON.stringify({ "message": "Employee created successfully", "employee_id": `${data.id}` }))
-            }).catch((err) => {
-                res.send(`Error: ${err}`)
-            })
-        } else {
-            res.send(JSON.stringify({ "status": false, "message": "Entered email already exists." }))
-        }
-    } catch (err) {
-        res.send(e)
-    }
+const createEmployee = async (employeeData) => {
+    let { first_name, last_name, email, position, salary, date_of_joining, department } = employeeData
+    let existing_emp = await findEmployeeE(email)
+    // if the emp found is null then create new employee
+    if (!existing_emp) {
+        throw new Error("Employee with the email already exists.")
+    } 
+    let new_emp = Employee({ first_name: first_name, last_name: last_name, email: email, position: position, salary: salary, date_of_joining: date_of_joining, department: department })
+    return await new_emp.save();
+
 }
 
 // get specific employee
-const getEmployee = async (req, res) => {
-    try {
-        let emp = await Employee.findById(req.params.id)
-
-        // if the list is not null then return all the employees
-        if (emp !== null) {
-            try {
-                res.status(200).send(JSON.stringify(emp))
-            } catch (e) {
-                res.error(e)
-            }
-        } else {
-            res.send(JSON.stringify({ "status": false, "message": "Employee with given id does not exists." }))
-        }
-    } catch (e) {
-        res.send(e)
+const getEmployee = async (id) => {
+    let emp = await Employee.findById(id)
+    if(!emp){
+        throw new Error("Employee doesn't exists")
     }
+    return emp;
 }
 
-const updateEmployee = async (req, res) => {
+const updateEmployee = async (id, updatedEmp) => {
     // try to find the employee from id if it exists then update it or else return err.
-    try {
-        let emp = await Employee.findById(req.params.eid)
-        if (emp === null) {
-            return res.status(404).send("Invalid email id.")
-        }
-
-        let employee = await Employee.findOneAndUpdate({ _id: req.params.eid }, { ...req.body, updated_at: new Date() }, { new: true })
-
-        console.log(employee)
-
-        res.status(200).send(JSON.stringify({ "message": "Employee details updated successfully" }))
-    } catch (e) {
-        res.send(e)
+    let emp = await getEmployee(id)
+    if (!emp) {
+        throw new Error("Employee doesn't exists")
     }
+    return await Employee.findOneAndUpdate({ _id: id }, { ...updatedEmp, updated_at: new Date() }, { new: true })
 }
 
 // delete employee from id
-const deleteEmployee = async (req, res) => {
-    // try to find the emp from id if exists then delete or else return no employee with the given id.
-    try {
-        if (!mongoose.Types.ObjectId.isValid(req.query.eid)) {
-            return res.status(400).send('Invalid ID format');
-        }
-
-        const deletedUser = await Employee.findOneAndDelete({ _id: req.query.eid })
-
-        if (!deletedUser) {
-            return res.status(400).send('Employee not found')
-        }
-        res.status(204).send(JSON.stringify({ "message": "Employee delete successfully." }))
-    } catch (e) {
-        res.status(500).send(e)
-    }
-
+const deleteEmployee = async (id) => {
+    return await Employee.findOneAndDelete({ _id: id })
 }
 
-const searchEmployee = async (req, res, next) => {
-    try {
-        let search_query = req.params.search_query
-        const results = await Employee.find({
-            $or: [
-                { position: { $regex: search_query, $options: 'i' } },
-                { department: { $regex: search_query, $options: 'i' } },
-            ],
-        });
-        res.send(JSON.stringify(results));
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+const searchEmployee = async (search_query) => {
+    return await Employee.find({
+        $or: [
+            { position: { $regex: search_query, $options: 'i' } },
+            { department: { $regex: search_query, $options: 'i' } },
+        ],
+    });    
 }
 
 // export functions to employee controller
